01e106a76b6859fa271c3686d82b77459fa85d84
unknown <laxminarayanajharapla27@gmail.com>
2025-07-01 08:06:26 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from pypdf import PdfMerger
+import os
+from tempfile import NamedTemporaryFile
+
+st.set_page_config(page_title="PDF Merger", layout="centered")
+
+st.title("PDF Merger Tool")
+st.write("Upload multiple pdf files and merge them into one.")
+
+upload_pdf = st.file_uploader("Choose first pdf", "pdf", accept_multiple_files=True)
+
+def main():
+    if upload_pdf is not None:
+        if st.button("Merge PDFs"):
+            merger = PdfMerger()
+
+            for pdf_file in upload_pdf:
+                merger.append(pdf_file)
+            
+            with NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+                merger.write(tmp_file.name)
+                merger.close()
+
+                with open(tmp_file.name, "rb") as f:
+                    st.download_button(
+                        label="Download Merged pdf",
+                        data=f,
+                        file_name="merged_output.pdf",
+                        mime="application/pdf"
+                    )
+            
+            st.success("PDFs merged successfully!")
+
+if __name__=="__main__":
+    main()
